4eb53a4aa65006555347c4c2c66c97d5216b1805
Vladimir Gorbunov <vsg@suburban.me>
2021-06-01 22:07:59 +0300

    Release 0.2.2

--- setup.py
+++ setup.py
@@ -41,7 +41,7 @@ with open(readme, 'r', encoding='utf-8') as f:
 
 setup(
     name='python-tkvdb',
-    version='0.2.1',
+    version='0.2.2',
     author='Vladimir Gorbunov',
     author_email='vsg@suburban.me',
     url='https://github.com/vladimir-g/python-tkvdb/',
